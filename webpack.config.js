aaf3840b3ba44933f62e327ecbcd9f52449b7199
gogoend <gogoend@qq.com>
2020-06-09 14:16:16 +0800

    html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -12,9 +12,22 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'react app',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 };
