431219864d5b1039132210fd9439d423c669e665
gogoend <gogoend@qq.com>
2020-09-05 08:08:16 +0800

    WIP: enhance: 尝试将首页改写为Hook组件 - 无奈有bug，更改下拉框选项后，页面显示结果是上一次的改变

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import React,{useState,useEffect} from 'react'
+import { Route, Link, useHistory, withRouter } from 'react-router-dom'
 
 import WeatherPage from './pages/weather-page'
 import Covid19Page from './pages/covid-19-page'
@@ -68,52 +68,51 @@ function getInfoById(dict, id, field) {
   return result
 }
 
-class App extends React.Component {
-  constructor(prop) {
-    super()
-    this.state = {
-      globalCurrentCity: getInfoById(cityList, 110000, 'code')[0]
-    }
-  }
-  globalCityChange(e) {
+function App (props){
+  const history = useHistory()
+  const [globalCurrentCity,setGlobalCurrentCity] = useState(getInfoById(cityList, 110000, 'code')[0])
+
+  const globalCityChange = (e) => {
     let cityCode = Number(e.target.value)
-    this.setState({
-      globalCurrentCity: getInfoById(cityList, cityCode, 'code')[0]
-    }, () => {
-      this.props.history.push(`/home/${this.state.globalCurrentCity.code}`)
-    })
+    setGlobalCurrentCity(getInfoById(cityList, cityCode, 'code')[0])
+    history.push(`/home/${globalCurrentCity.code}`)
   }
-  render() {
 
-    let cityOptionDOM = cityList.map((item, index) => (<option key={index} value={item.code}>{item.city}</option>))
+  let cityOptionDOM = cityList.map((item, index) => (<option key={index} value={item.code}>{item.city}</option>))
 
-    return (
-      <Provider value={{
-        globalCurrentCity: this.state.globalCurrentCity
-      }}>
-        <div className={style['app-wrap']}>
-          <nav>
-            <h1>{this.state.globalCurrentCity.city}</h1>
-            <section className={style['city-select']}>
-              <select onChange={(e) => this.globalCityChange(e)} value={this.state.globalCurrentCity.code}>
-                {cityOptionDOM}
-              </select>
-            </section>
-          </nav>
-          {/* <Route path="/" exact component={WeatherPage} /> */}
-          <Route path="/home/:cityId" component={WeatherPage}></Route>
-          <Route path="/covid-19/:cityId" component={Covid19Page}></Route>
-          {/* <WeatherPage city={this.state.globalCurrentCity} /> */}
-          <nav>
-            <Link
-              to={`/home/${this.state.globalCurrentCity.code}`}
-            >天气</Link>
-            <Link to={`/covid-19/${this.state.globalCurrentCity.city}`}>COVID-19</Link>
-          </nav>
-        </div>
-      </Provider>
-    )
-  }
+  console.log('1')
+
+  // useEffect(()=>{
+    document.title = globalCurrentCity.city
+  //   return ()=>{}
+  // })
+
+
+  return (<Provider value={{
+    globalCurrentCity: globalCurrentCity
+  }}>
+    <div className={style['app-wrap']}>
+      <nav>
+        <h1>{globalCurrentCity.city}</h1>
+        <section className={style['city-select']}>
+          <select onChange={(e) => globalCityChange(e)} value={globalCurrentCity.code}>
+            {cityOptionDOM}
+          </select>
+        </section>
+      </nav>
+      {/* <Route path="/" exact component={WeatherPage} /> */}
+      <Route path="/home/:cityId" component={WeatherPage}></Route>
+      <Route path="/covid-19/:cityId" component={Covid19Page}></Route>
+      {/* <WeatherPage city={globalCurrentCity} /> */}
+      <nav>
+        <Link
+          to={`/home/${globalCurrentCity.code}`}
+        >天气</Link>
+        <Link to={`/covid-19/${globalCurrentCity.city}`}>COVID-19</Link>
+      </nav>
+    </div>
+  </Provider>)
+
 }
 
 export {
